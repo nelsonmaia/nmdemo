28d1e0922130ba5b19e6a160cd6d3ba949feecf1
nellson.maia@gmail.com <nellson.maia@gmail.com>
2018-09-19 10:56:27 +0100

    Adding different headers for B2B case

--- src/views/LandingPage/Sections/Welcome.jsx
+++ src/views/LandingPage/Sections/Welcome.jsx
@@ -28,15 +28,19 @@ class Welcome extends React.Component {
                         <h1 className={classes.title}>
                             {userGroups.includes('marketing') ?
                                 ("Marketing is the future") :
-                                auth.isAuthenticated() ? userRoles.includes("Federated User") ? null : ("The authentication and authorization has began") :
+                                auth.isAuthenticated() ? userRoles.includes("Federated User")  || userRoles.includes("Business User")  ? null : ("The authentication and authorization has began") :
                                     ("Your Story Starts With Us.")}
                         </h1>
+                        
 
                     ) : (
                             <h1 className={classes.title}>
                                 Welcome to this journey!
                             </h1>
                         )}
+                        {userRoles.includes("Business User") ? (
+                            <h1 className={classes.title}>This is a B2B case</h1>
+                        ) : null}
                         {userRoles && !userRoles.includes("Federated User") ? (
                              <h4>
                              Bacon ipsum dolor amet shank porchetta boudin chicken. Tenderloin bacon bresaola,
@@ -45,7 +49,7 @@ class Welcome extends React.Component {
      
                          </h4>
                         ): null}
-
+                        
                    
                     {auth.isAuthenticated() && !isSocial  && !userRoles.includes("Federated User") ? (
                         <h4>User groups are: {JSON.stringify(userGroups, null, '  ')}</h4>
