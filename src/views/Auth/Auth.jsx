c1797f07558d76cac2a49c1801365820646dfad8
nellson.maia@gmail.com <nellson.maia@gmail.com>
2018-11-01 12:19:04 +0000

    Changing only for id_token

--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -23,7 +23,7 @@ export default class Auth {
     this.auth0.authorize({
       redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
       audience: process.env.REACT_APP_AUTH0_AUDIENCE,
-      responseType: 'token id_token',
+      responseType: 'id_token',
       scope: 'openid profile email '
     });
   }
